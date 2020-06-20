a66bcb11c383c21374bcd8afea3ed8d69d75786e
kelvzxu <ceokelvin12@gmail.com>
2020-06-21 00:58:01 +0700

    [FIX] Bugs Icon fa-caret-right

--- resources/views/accounting/report/general_ledger.blade.php
+++ resources/views/accounting/report/general_ledger.blade.php
@@ -135,25 +135,16 @@
 @endsection
 @section('js')
 <script>
-    function show(element){
-    $(element).css( "display", "" );
-}
-function unactive(){
-    $('.nav-link').removeClass('active');
-}
- 
     $(".o_group_has_content").click(function() {
-        // console.log
-        // var item = document.querySelector('.'+this.id);
-        // console.log(item);
-        // if (item.style.display === 'none') {
-        //     item.style.display = 'block';
-        // } else {
-        //     item.style.display = 'none';
-        // }
         if($('.' + this.id).css('display')=='none'){
+            $(this).children("th").find("span").removeClass('fa-caret-right');
+            $(this).children("th").find("span").addClass('fa-caret-down');
+            $(this).addClass('o_group_open');
             console.log($('.' + this.id).css("display", "" ));
         }else{
+            $(this).children("th").find("span").addClass('fa-caret-right');
+            $(this).children("th").find("span").removeClass('fa-caret-down');
+            $(this).removeClass('o_group_open');
             console.log($('.' + this.id).css("display", "none" ));
         }
     });
