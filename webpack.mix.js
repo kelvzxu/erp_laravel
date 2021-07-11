8c725052d71b89c0bc0c6d9cdb585563cbc90a10
kelvzxu <ceokelvin12@gmail.com>
2021-07-11 14:07:58 +0700

    [Add] bootstrap and fontawesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/web_assets_apps.js', 'public/js')
+    .sass('resources/sass/web_assets_apps.scss', 'public/css')
+    .sourceMaps();
