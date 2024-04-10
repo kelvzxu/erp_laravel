0dc58cd9bda91b56a9b53e91d4018ac2371f3ea4
kelvzxu <ceokelvin12@gmail.com>
2024-04-10 16:36:41 +0700

    [ADD] MIgrate to laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BaseController;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', [BaseController::class, 'AuthorizesDatabase']);
-    Route::group(['prefix'=>'web'],function(){
-        Route::group(['prefix'=>'database'],function(){
-            Route::get('/create', [BaseController::class, 'CreateDatabase'])->name('CreateDatabase');
-        });
-        Route::group(['prefix'=>'datasource'],function(){
-            Route::get('/registration', [BaseController::class, 'DatasourceRegistration'])->name('RegisterDatasource');
-            Route::post('/save', [BaseController::class, 'DatasourceSaved'])->name('SaveDatasource');
-    });
+Route::get('/', function () {
+    return "okok";
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
